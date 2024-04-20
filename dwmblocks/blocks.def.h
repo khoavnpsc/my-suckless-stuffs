b23597e1fefc074db3b62028c97e60c222efb0e0
Khoa <khoavnpsc@gmail.com>
2024-04-20 17:52:59 +1000

    Initial commit

---
+++ dwmblocks/blocks.def.h
@@ -0,0 +1,15 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{" Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+
+	{"Bat: ", "cat /sys/class/power_supply/BAT0/capacity | awk '{ print $1\"%\" }'",	30,		0},
+	
+	{"", "cat /sys/class/power_supply/BAT0/status",	30,		0},
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
